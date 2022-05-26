54ce2ad47ea4a7c53621e7929d9f833750d8193c
jefex7 <duartepireslopes@gmail.com>
2022-05-26 15:11:46 +0100

    add customisation to lab

--- src/main/java/uk/gov/hmcts/labs/jefex7/controllers/RootController.java
+++ src/main/java/uk/gov/hmcts/labs/jefex7/controllers/RootController.java
@@ -26,7 +26,8 @@ public class RootController {
     })
     @RequestMapping(value = "/", method = GET, produces = TEXT_PLAIN_VALUE)
     
-    public ResponseEntity<String> welcome() {
-        return ok("Welcome to labs-jefex7 application");
-    }
+     public ResponseEntity<String> welcome() {
+     return ok("Welcome to your app, my favourite fruit is " +  System.getenv("FAVOURITE_FRUIT"));
+     }
 }
+ 
